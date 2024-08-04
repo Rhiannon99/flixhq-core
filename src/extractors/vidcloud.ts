3933c2ea31f5d05ae122ab1d4f1ac88da278d2ef
Rhiannon99 <78912148+Rhiannon99@users.noreply.github.com>
2024-08-04 18:10:20 +0200

    Update vidcloud.ts

--- src/extractors/vidcloud.ts
+++ src/extractors/vidcloud.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import CryptoJS from 'crypto-js';
-import {IVideoResult} from '../types/types';
-import {isJson} from '../utils';
+import { IVideoResult } from '../types/types';
+import { isJson } from '../utils';
 
 class VidCloud {
     protected serverName = 'VidCloud';
@@ -12,7 +12,7 @@ class VidCloud {
         const videoResult: IVideoResult = {
             sources: [],
             subtitles: [],
-        }
+        };
 
         try {
             const id = videoUrl.href.split('/').pop()?.split('?')[0];
@@ -22,20 +22,20 @@ class VidCloud {
                     'Referer': videoUrl.href,
                 },
             };
-            const {data} = await axios.get(`${isAlternative ? this.host2 : this.host}/ajax/embed-4/getSources?id=${id}`, options);
+            const { data } = await axios.get(`${isAlternative ? this.host2 : this.host}/ajax/embed-4/getSources?id=${id}`, options);
 
             let sources = null;
 
             if (!isJson(data.sources)) {
                 let keys = await (await axios.get('https://keys4.fun')).data["rabbitstream"]["keys"];
                 const keyString = btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(keys))));
-                const decryptedVal = CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8);
-                sources = JSON.parse(CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8));
-                sources = isJson(decryptedVal) ? JSON.parse(decryptedVal) : res.data.sources;
+                const decryptedVal = CryptoJS.AES.decrypt(data.sources, keyString).toString(CryptoJS.enc.Utf8);
+                sources = JSON.parse(decryptedVal);
+                sources = isJson(decryptedVal) ? JSON.parse(decryptedVal) : data.sources;
             }
 
             for (const source of sources) {
-                const {data} = await axios.get(source.file, options);
+                const { data } = await axios.get(source.file, options);
                 const videoUrls = data.split('\n').filter((line: string) => line.includes('.m3u8')) as string[];
                 const videoQualities = data.split('\n').filter((line: string) => line.includes('RESOLUTION=')) as string[];
 
@@ -55,7 +55,7 @@ class VidCloud {
                 return {
                     url: track.file,
                     lang: track.label ?? 'Default',
-                }
+                };
             });
 
             return videoResult;
